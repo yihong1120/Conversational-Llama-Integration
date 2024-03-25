e4cd511f7fb3d0d50e8d553b962fd4683a83329b
yihong1120 <qazsx748596@gmail.com>
2024-03-26 02:19:14 +0800

    Database info

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,1 +1,7 @@
-# configuration
+# Database configuration
+DB_PARAMS = {
+    "dbname": "your_database_name",
+    "user": "your_database_user",
+    "password": "your_database_password",
+    "host": "your_database_host"
+}
